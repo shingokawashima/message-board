ec97db11deb0b0203c54ddf20d32dab78baf2bc2
shingokawashima <shingokawashima@gmail.com>
2015-09-26 04:03:15 +0000

    Challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20} , presence: true
   validates :body , length: {minimum: 2, maximum: 30 }, presence: true
+  validates :age , presence: true, numericality: { only_integer:true, greater_than: 1}
 end
